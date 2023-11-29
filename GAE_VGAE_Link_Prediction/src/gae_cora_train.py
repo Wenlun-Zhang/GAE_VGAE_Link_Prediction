1dfcd3e10dc8cf390d7a265832a60cbae3fd1d11
Wenlun Zhang <141137353+Wenlun-Zhang@users.noreply.github.com>
2023-11-29 23:43:09 +0900

    Add files via upload

---
+++ GAE_VGAE_Link_Prediction/src/gae_cora_train.py
@@ -0,0 +1,80 @@
+import os
+import torch
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+from torch_geometric.nn import GCNConv
+from torch_geometric.utils import train_test_split_edges
+from torch_geometric.nn import GAE
+
+os.environ['TORCH'] = torch.__version__
+print(torch.__version__)
+
+# Load data
+
+dataset = Planetoid(r'E:\Machine_Learning\Dataset\Processing', 'cora', transform=T.NormalizeFeatures())
+data = dataset[0]
+data.train_mask = data.val_mask = data.test_mask = None
+print(data)
+
+data = train_test_split_edges(data)
+print(data)
+
+# Define Encoder
+
+
+class GCNEncoder(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GCNEncoder, self).__init__()
+        self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=True)    # Cached only for transductive learning
+        self.conv2 = GCNConv(2 * out_channels, out_channels, cached=True)   # Cached only for transductive learning
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        return self.conv2(x, edge_index)
+
+
+# Define Autoencoder
+
+# Parameters
+out_channels = 2
+num_features = dataset.num_features
+epochs = 100
+
+# Model
+model = GAE(GCNEncoder(num_features, out_channels))
+
+# GPU setup
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+x = data.x.to(device)
+train_pos_edge_index = data.train_pos_edge_index.to(device)
+
+# Initialize optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+
+def train():
+    model.train()
+    optimizer.zero_grad()
+    z = model.encode(x, train_pos_edge_index)
+    loss = model.recon_loss(z, train_pos_edge_index)
+    loss.backward()
+    optimizer.step()
+    return float(loss)
+
+
+def test(pos_edge_index, neg_edge_index):
+    model.eval()
+    with torch.no_grad():
+        z = model.encode(x, train_pos_edge_index)
+    return model.test(z, pos_edge_index, neg_edge_index)
+
+
+for epoch in range(1, epochs + 1):
+    loss = train()
+
+    auc, ap = test(data.test_pos_edge_index, data.test_neg_edge_index)
+    print('Epoch: {:03d}, AUC: {:.4f}, AP: {:.4f}'.format(epoch, auc, ap))
+
+Z = model.encode(x, train_pos_edge_index)
+print(Z)
---
+++ GAE_VGAE_Link_Prediction/src/vgae_citeceer_train.py
@@ -0,0 +1,82 @@
+import os
+import torch
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+from torch_geometric.nn import GCNConv
+from torch_geometric.utils import train_test_split_edges
+from torch_geometric.nn import VGAE
+
+os.environ['TORCH'] = torch.__version__
+print(torch.__version__)
+
+# Load data
+
+dataset = Planetoid(r'E:\Machine_Learning\Dataset\Processing', 'CiteSeer', transform=T.NormalizeFeatures())
+data = dataset[0]
+data.train_mask = data.val_mask = data.test_mask = None
+print(data)
+
+data = train_test_split_edges(data)
+print(data)
+
+# Define Encoder
+
+
+class VariationalGCNEncoder(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(VariationalGCNEncoder, self).__init__()
+        self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=True)    # Cached only for transductive learning
+        self.conv_mu = GCNConv(2 * out_channels, out_channels, cached=True)
+        self.conv_logstd = GCNConv(2 * out_channels, out_channels, cached=True)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        return self.conv_mu(x, edge_index), self.conv_logstd(x, edge_index)
+
+
+# Define Autoencoder
+
+# Parameters
+out_channels = 2
+num_features = dataset.num_features
+epochs = 300
+
+# Model
+model = VGAE(VariationalGCNEncoder(num_features, out_channels))
+
+# GPU setup
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+x = data.x.to(device)
+train_pos_edge_index = data.train_pos_edge_index.to(device)
+
+# Initialize optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+
+def train():
+    model.train()
+    optimizer.zero_grad()
+    z = model.encode(x, train_pos_edge_index)
+    loss = model.recon_loss(z, train_pos_edge_index)
+    loss = loss + (1 / data.num_nodes) * model.kl_loss()
+    loss.backward()
+    optimizer.step()
+    return float(loss)
+
+
+def test(pos_edge_index, neg_edge_index):
+    model.eval()
+    with torch.no_grad():
+        z = model.encode(x, train_pos_edge_index)
+    return model.test(z, pos_edge_index, neg_edge_index)
+
+
+for epoch in range(1, epochs + 1):
+    loss = train()
+
+    auc, ap = test(data.test_pos_edge_index, data.test_neg_edge_index)
+    print('Epoch: {:03d}, AUC: {:.4f}, AP: {:.4f}'.format(epoch, auc, ap))
+
+Z = model.encode(x, train_pos_edge_index)
+print(Z)
